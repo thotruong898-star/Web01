a3558be1ce7e22059a8e01b454e190259c652813
tho <thotruong898@gmail.com>
2025-09-23 22:54:31 +0700

    '23/9/2025'

--- my-app/src/app/(Home)/(Footer)/Footer.tsx
+++ my-app/src/app/(Home)/(Footer)/Footer.tsx
@@ -1,3 +1,94 @@
-export default function FooterPage(){
-    return(<div>Footer</div>)
+export default function FooterPage() {
+  return (
+    <div >
+    
+      <div className="footer1">
+        <div>
+          <p>
+            <strong>Tổng đài hổ trợ</strong>
+          </p>
+          <p>
+            <span>Gọi mua: 0356943305</span>
+          </p>
+          <p>
+            <span>khiếu nại: 0356943305</span>
+          </p>
+          <p>
+            <span>Bảo hành: 0356943305</span>
+          </p>
+        </div>
+        <div>
+          <p>
+            <strong>Về công ty</strong>
+          </p>
+          <p>
+            <span>Giới thiệu công ty</span>
+          </p>
+          <p>
+            <span>Tuyển dụng</span>
+          </p>
+          <p>
+            <span>Gữi góp ý khiếu nại</span>
+          </p>
+        </div>
+        <div>
+          <p>
+            <strong>Thông tin khác</strong>
+          </p>
+          <p>
+            <span>Tích điểm quà tặng VIP</span>
+          </p>
+          <p>
+            <span>Lịch sữ mua hàng</span>
+          </p>
+          <p>
+            <span>Đăng kí bán hàng CTV chiết khấu cao</span>
+          </p>
+          <p>
+            <span>Tìm hiểu mua hàng trả chậm</span>
+          </p>
+          <p>
+            <span>Chính sách bảo hành</span>
+          </p>
+
+          <p>
+            <span>Xem thêm</span>
+          </p>
+        </div>
+        <div>
+          <p>
+            <strong>Website cùng tập đoàn</strong>
+          </p>
+          <p>
+            <span>Giới thiệu công ty</span>
+          </p>
+          <p>
+            <span>Tuyển dụng</span>
+          </p>
+          <p>
+            <span>Gữi góp ý khiếu nại</span>
+          </p>
+        </div>
+      </div>
+
+
+      <div className="copyright">
+        <section>
+          <p>
+            © 2018. Công ty cổ phần Thế Giới Di Động. GPDKKD: 0303217354 do sở
+            KH & ĐT TP.HCM cấp ngày 02/01/2007. GPMXH: 21/GP-BTTTT do Bộ Thông
+            Tin và Truyền Thông cấp ngày 11/01/2021.
+            <br />
+            Địa chỉ: 128 Trần Quang Khải, P.Tân Định, TP. Hồ Chí Minh. Địa chỉ
+            liên hệ và gửi chứng từ: Lô T2-1.2, Đường D1, P.Tăng Nhơn Phú, TP.Hồ
+            Chí Minh. Điện thoại: 028 38125960. Email:
+            hotrotmdt@thegioididong.com. Chịu trách nhiệm nội dung: Huỳnh Văn
+            Tốt. Đại diện ủy quyền giải quyết các vấn đề liên quan bảo vệ quyền
+            lợi người tiêu dùng: Huỳnh Văn Tốt. Email:
+            hotrotmdt@thegioididong.com. Xem chính sách sử dụng
+          </p>
+        </section>
+      </div>
+    </div>
+  );
 }
--- my-app/src/app/(product)/product/page.tsx
+++ my-app/src/app/(product)/product/page.tsx
@@ -1,10 +1,234 @@
 import Main from "@/app/(Home)/(Main)/Main";
+import anh from "../../(Home)/(Header)/images/img1.png";
+import Styles from "./product.module.css";
+import React from "react";
+import Image from "next/image";
+import realmeC75 from "./img/realmeC75.jpg";
+import { styleText } from "node:util";
 
-export default function Product(){
-    return (
-        <div>
-            <Main><h1>Đây là trang sản phẩm</h1></Main>
+export default function Product({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="main">
+      <div className="main-test1">
+        <Main>
+          {" "}
+          <Image src={anh} alt={"anh"} height={240} width={1200} />
+          <div className="promotion">
+            <h3>Khuyến mãi Online</h3>
+            <ul>
+              <li>FLAST SALE</li>
+              <li>ONLINE ONLY</li>
+              <li>Điện Thoại</li>
+              <li>Apple</li>
+              <li>Laptop</li>
+              <li>Phụ Kiện</li>
+              <li>Đồng Hồ</li>
+              <li>PC, Máy in</li>
+            </ul>
+          </div>
+          <div className="promotion">
+            <ul>
+              <li>Chỉ còn :</li>
+              <li>Sắp diễn ra </li>
+              <li>Ngày mai</li>
+              <li>Ngày mai</li>
+              <li>Ngày mai</li>
+            </ul>
+          </div>
+          <div className="list-product">
+            <div className="item">
+              <div style={{ margin: "0 5px" }}>
+                <div className={Styles.itemImg}>
+                  <Image src={realmeC75} alt={""} height={172} width={155} />
+                </div>
+                <h3 className={Styles.itemH3}>realme C75 8GB/512GB</h3>
+                <strong className={Styles.price}>
+                  6.540.000₫
+                  <span className={Styles.priceAndDiscount}>
+                    <label htmlFor="">8.300.000₫</label>
+                    <small>-21%</small>
+                  </span>
+                </strong>
+                <div className={Styles.contait}>
+                  <Image
+                    src={"/flash-sale.png"}
+                    alt={""}
+                    height={22}
+                    width={20}
+                    className={Styles.img}
+                  />
+                  <span className={Styles.count}>
+                    <b>Còn 6/10 suất</b>
+                  </span>
+                </div>
+                <div className={Styles.btnBuy}>
+                  <a href="#!">Mua ngay</a>
+                </div>
+              </div>
+            </div>
+            {/* <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div> */}
+
+            <div className="item">
+              <div style={{ margin: "0 5px" }}>
+                <div className={Styles.itemImg}>
+                  <Image src={realmeC75} alt={""} height={172} width={155} />
+                </div>
+                <h3 className={Styles.itemH3}>realme C75 8GB/512GB</h3>
+                <strong className={Styles.price}>
+                  6.540.000₫
+                  <span className={Styles.priceAndDiscount}>
+                    <label htmlFor="">8.300.000₫</label>
+                    <small>-21%</small>
+                  </span>
+                </strong>
+                <div className={Styles.contait}>
+                  <Image
+                    src={"/flash-sale.png"}
+                    alt={""}
+                    height={22}
+                    width={20}
+                    className={Styles.img}
+                  />
+                  <span className={Styles.count}>
+                    <b>Còn 6/10 suất</b>
+                  </span>
+                </div>
+                <div className={Styles.btnBuy}>
+                  <a href="#!">Mua ngay</a>
+                </div>
+              </div>
+            </div>
+            <div className="item">
+              <div style={{ margin: "0 5px" }}>
+                <div className={Styles.itemImg}>
+                  <Image src={realmeC75} alt={""} height={172} width={155} />
+                </div>
+                <h3 className={Styles.itemH3}>realme C75 8GB/512GB</h3>
+                <strong className={Styles.price}>
+                  6.540.000₫
+                  <span className={Styles.priceAndDiscount}>
+                    <label htmlFor="">8.300.000₫</label>
+                    <small>-21%</small>
+                  </span>
+                </strong>
+                <div className={Styles.contait}>
+                  <Image
+                    src={"/flash-sale.png"}
+                    alt={""}
+                    height={22}
+                    width={20}
+                    className={Styles.img}
+                  />
+                  <span className={Styles.count}>
+                    <b>Còn 6/10 suất</b>
+                  </span>
+                </div>
+                <div className={Styles.btnBuy}>
+                  <a href="#!">Mua ngay</a>
+                </div>
+              </div>
+            </div>
+            <div className="item">
+              <div style={{ margin: "0 5px" }}>
+                <div className={Styles.itemImg}>
+                  <Image src={realmeC75} alt={""} height={172} width={155} />
+                </div>
+                <h3 className={Styles.itemH3}>realme C75 8GB/512GB</h3>
+                <strong className={Styles.price}>
+                  6.540.000₫
+                  <span className={Styles.priceAndDiscount}>
+                    <label htmlFor="">8.300.000₫</label>
+                    <small>-21%</small>
+                  </span>
+                </strong>
+                <div className={Styles.contait}>
+                  <Image
+                    src={"/flash-sale.png"}
+                    alt={""}
+                    height={22}
+                    width={20}
+                    className={Styles.img}
+                  />
+                  <span className={Styles.count}>
+                    <b>Còn 6/10 suất</b>
+                  </span>
+                </div>
+                <div className={Styles.btnBuy}>
+                  <a href="#!">Mua ngay</a>
+                </div>
+              </div>
+            </div>
+            <div className="item">
+              <div style={{ margin: "0 5px" }}>
+                <div className={Styles.itemImg}>
+                  <Image src={realmeC75} alt={""} height={172} width={155} />
+                </div>
+                <h3 className={Styles.itemH3}>realme C75 8GB/512GB</h3>
+                <strong className={Styles.price}>
+                  6.540.000₫
+                  <span className={Styles.priceAndDiscount}>
+                    <label htmlFor="">8.300.000₫</label>
+                    <small>-21%</small>
+                  </span>
+                </strong>
+                <div className={Styles.contait}>
+                  <Image
+                    src={"/flash-sale.png"}
+                    alt={""}
+                    height={22}
+                    width={20}
+                    className={Styles.img}
+                  />
+                  <span className={Styles.count}>
+                    <b>Còn 6/10 suất</b>
+                  </span>
+                </div>
+                <div className={Styles.btnBuy}>
+                  <a href="#!">Mua ngay</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="item">
+              <div style={{ margin: "0 5px" }}>
+                <div className={Styles.itemImg}>
+                  <Image src={realmeC75} alt={""} height={172} width={155} />
+                </div>
+                <h3 className={Styles.itemH3}>realme C75 8GB/512GB</h3>
+                <strong className={Styles.price}>
+                  6.540.000₫
+                  <span className={Styles.priceAndDiscount}>
+                    <label htmlFor="">8.300.000₫</label>
+                    <small>-21%</small>
+                  </span>
+                </strong>
+                <div className={Styles.contait}>
+                  <Image
+                    src={"/flash-sale.png"}
+                    alt={""}
+                    height={22}
+                    width={20}
+                    className={Styles.img}
+                  />
+                  <span className={Styles.count}>
+                    <b>Còn 6/10 suất</b>
+                  </span>
+                </div>
+                <div className={Styles.btnBuy}>
+                  <a href="#!">Mua ngay</a>
+                </div>
+              </div>
+            </div>
             
-        </div>
-    )
+            
+          </div>
+        </Main>
+      </div>
+    </div>
+  );
 }
